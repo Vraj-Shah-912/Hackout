ab0dde3f1b49d79e80548920fee29e5c457ad9f8
aryan patel <210130107060@gecg28.ac.in>
2023-08-26 13:55:45 +0530

    userprofiledone

--- DigitalHospital/login/admin.py
+++ DigitalHospital/login/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from login.models import UserProfile
+class UserProfileAdmin (admin.ModelAdmin):
+    list_display=('user','role')
+
+admin.site.register(UserProfile,UserProfileAdmin)
